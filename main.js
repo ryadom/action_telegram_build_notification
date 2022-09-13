f938649a92e6ef78379c362a0ccb7d9e5624aaef
Mark Riabov <5471316+ryadom@users.noreply.github.com>
2022-09-13 13:40:07 +0400

    initial implementation for push events

---
+++ main.js
@@ -0,0 +1,62 @@
+const axios = require('axios');
+const github = require('@actions/github');
+const core = require('@actions/core');
+
+async function send_message(token, chatId, text) {
+    return axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
+        chat_id: chatId,
+        text: text,
+        parse_mode: 'MarkdownV2',
+        disable_web_page_preview: 'True',
+    })
+}
+
+function escape(text) {
+    if (!text) {
+        return ''
+    }
+    text = String(text);
+    for (let c of '.-{}=()_*[]~`>#+|!') {
+        text = text.replaceAll(c, '\\' + c);
+    }
+    return text;
+}
+
+(async function run() {
+    try {
+        const telegramToken = core.getInput('token');
+        const chatId = core.getInput('chat');
+        const status = core.getInput('status');
+        const payload = github.context.payload;
+        const repoName = payload.repository.name;
+        const repoUrl = payload.repository.url;
+        const { ref, runId, workflow } = github.context;
+        let statusIcon;
+        switch (status) {
+            case 'success': statusIcon = '✅'; break;
+            case 'failure': statusIcon = '🔴'; break;
+            default: statusIcon = '⚠️'; break;
+        }
+        core.debug(JSON.stringify(payload));
+        if (github.context.eventName == 'push') {
+            if (!ref.startsWith('refs/heads/')) {
+                core.setFailed(`can't parse ref ${ref}`);
+                return;
+            }
+            const branchName = ref.substring(11);
+            const branchUrl = `${repoUrl}/tree/${branchName}`;
+            const commitMessage = payload.head_commit.message;
+            const commitAuthor = payload.head_commit.author.username;
+            let message = `${statusIcon} [${escape(repoName)}/${escape(branchName)}](${escape(branchUrl)}) ${escape(workflow)} *${escape(status)}*
+
+\`\`\`${escape(commitMessage)}\`\`\` by [${escape(commitAuthor)}](http://github.com/${escape(commitAuthor)})
+
+[View details](${escape(repoUrl)}/actions/runs/${escape(runId)})`;
+            await send_message(telegramToken, chatId, message);    
+        } else {
+            core.setFailed(`unsupported eventName ${github.context.eventName}`);
+        }
+    } catch (error) {
+      core.setFailed(error.message);
+    }
+  })();
